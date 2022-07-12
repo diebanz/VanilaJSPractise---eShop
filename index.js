24a8204123b210328bf5eb6861b809eec882779d
Natalia Banz <banznatalia@gmail.com>
2022-07-12 11:59:23 +0200

    Creating a clients with class Client, and ClientsManager

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "strict mode";
 
-// Creating a class for shop
+////////////////////////////// Creating a class for shop //////////////////////////////
 
 class EShop {
 	constructor(name, owner, address, billingInfo) {
@@ -14,7 +14,7 @@ class EShop {
 	}
 }
 
-// Creating a class for products
+////////////////////////////// Creating a class for products  //////////////////////////////
 
 class Product {
 	constructor(description, price, images, title, code) {
@@ -26,7 +26,7 @@ class Product {
 	}
 }
 
-// Creating a class for Product Manager
+////////////////////////////// Creating a class for Product Manager //////////////////////////////
 
 class ProductManager {
 	constructor(products) {
@@ -55,7 +55,7 @@ class ProductManager {
 	}
 }
 
-// Creating a company with the class Eshop
+////////////////////////////// CREATING A SHOP  //////////////////////////////
 
 const shop = "Simple stuff";
 
@@ -78,12 +78,12 @@ const simpleStuff = new EShop(
 	shopBillingInfo
 );
 
-////////// Creating the products with class Product
+////////////////////////////// CREATING THE PRODUCTS //////////////////////////////
 
 const painting = new Product(
 	"This is my first painting",
 	130,
-	["https://www.gevshop.com/?product_id=116086360_38"],
+	"https://www.gevshop.com/?product_id=116086360_38",
 	"Landscape from my village",
 	0001
 );
@@ -91,24 +91,75 @@ const painting = new Product(
 const handmadeToy = new Product(
 	"Selfmade Toy",
 	150,
-	[
-		"https://www.flipkart.com/destino-pink-unicorn-plush-toy-stuffed-animal-pillow-cushion-soft-toys-baby-kids-24-cm/p/itm00a6254e46e9c"
-	],
+
+	"https://www.flipkart.com/destino-pink-unicorn-plush-toy-stuffed-animal-pillow-cushion-soft-toys-baby-kids-24-cm/p/itm00a6254e46e9c",
 	"Unicorn",
 	0002
 );
 const ceramic = new Product(
 	"Ceramic Vase",
 	250,
-	["https://www.thoughtco.com/ceramic-definition-chemistry-4145312"],
-	"UVase",
+	"https://www.thoughtco.com/ceramic-definition-chemistry-4145312",
+	"Vase",
 	0003
 );
 
-/// ADDING PRODUCTS
+////////////////////////////// ADDING PRODUCTS //////////////////////////////
 
 const productsManager = new ProductManager(simpleStuff.products);
 productsManager.addItem(painting);
 productsManager.addItem(ceramic);
 productsManager.deleteItem(painting);
+
+////////////////////////////// CREATING A CLASS FOR A CLIENT //////////////////////////////
+
+class Client {
+	constructor(name, email, address, bday) {
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.bday = bday;
+	}
+}
+
+const jan = new Client("Jan", "john@email.com", "Berlin", "11/3/1970");
+const maria = new Client("Maria", "maria@email.com", "Munchen", "1/12/1984");
+const sebastian = new Client(
+	"Sebastian",
+	"sebastian@email.com",
+	"Heidenheim",
+	"1/3/1992"
+);
+
+////////////////////////////// CREATING A CLASS FOR A CLIENT MANAGER//////////////////////////////
+
+class ClientManager {
+	constructor(clients) {
+		this.clients = clients;
+	}
+
+	addClient(...clients) {
+		this.clients.push(...clients);
+	}
+
+	removeClient(client) {
+		const index = this.clients.findIndex(
+			(item) => item.name === client.name
+		);
+
+		if (index > -1) {
+			this.clients.splice(index, 1);
+		} else console.log("Client not found");
+	}
+
+	listClients() {
+		console.log("List all Clients:", this.clients);
+	}
+}
+
+const clientManager = new ClientManager(simpleStuff.clients);
+clientManager.addClient(jan, maria, sebastian);
+clientManager.removeClient(maria);
+clientManager.listClients();
+console.log("----------------------SHOP-------------------------");
 console.log(simpleStuff);
